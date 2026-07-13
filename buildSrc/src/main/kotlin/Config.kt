0135b9b9bcd729a1683a2e6b2f7c1e0155c9f8d5
Riad Mashrub Shourov <shourovrm@gmail.com>
2026-07-13 16:42:27 +0600

    chore: v0.1.9

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -7,6 +7,6 @@ object Config {
     const val compileSdk = 33
     const val targetSdk = 33
 
-    const val versionCode = 9
-    const val versionName = "0.1.8"
+    const val versionCode = 10
+    const val versionName = "0.1.9"
 }
